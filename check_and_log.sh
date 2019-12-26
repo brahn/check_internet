9e47458039b56035550a50d45f7051d1363d21d7
Benjamin Rahn <benjamin.rahn@gmail.com>
2019-12-26 14:54:31 -0500

    log results to file; add sample results

--- check_and_log.sh
+++ check_and_log.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+LOGFILE=$1
+if [ ! -f "$LOGFILE" ]; then
+	echo "$LOGFILE does not exist, creating..."
+	echo "Timestamp, Packet Loss, Average Ping Time, Units" > $LOGFILE
+fi
+
 ping_and_format_result () {
 	echo `ping -q -c 1 -W 1 google.com | awk '/packet loss/ {print $7} /round-trip/ {split($4, times, "/"); print times[2], $5}'`
 }
 
 now=$(date +%Y-%m-%d\ %T\ %Z)
 formatted_ping_result=$(ping_and_format_result)
-echo "$now $formatted_ping_result"
+echo "$now $formatted_ping_result" >> $LOGFILE
